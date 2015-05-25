5f03f6a768a1db9736bc99584d731e93642eeaca
Emilia Szymanska <szymanska.emilka@gmail.com>
2015-05-25 13:35:40 +0200

    Sample project

---
+++ ImageRecognition/Program.cs
@@ -0,0 +1,19 @@
+﻿// Edge detection by Canny algorithm
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+
+class Program
+{
+    static void Main()
+    {
+        Mat src = new Mat("Lena.png", LoadMode.GrayScale);
+        Mat dst = new Mat();
+
+        Cv2.Canny(src, dst, 50, 200);
+        using (new Window("src image", src))
+        using (new Window("dst image", dst))
+        {
+            Cv2.WaitKey();
+        }
+    }
+}
